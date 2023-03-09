90bf64e251b0e24cff9ea8ad5e0e270b43fe7e6a
Rafael Mendoza <rafael.mendoza64@gmail.com>
2023-03-09 22:35:18 +0100

    create prototype header component

---
+++ src/assets/Material.ts
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
+import Modal from '@mui/material/Modal';
+
+export const Material = {
+    Button,
+    Grid,
+    Modal
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,38 @@
+import React from 'react'
 import Head from 'next/head'
-import Header from '../components/Header'
+import Header from '@/components/Header'
+import SearchBar from '@/components/SearchBar'
+import { Icons } from '@/assets/Icons'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <React.Fragment>
       <Head>
         <title>Telecommunication</title>
         <meta name="description" content="Fullstack Demo App" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <Header />
+      <main>
+        
+        <Header
+          navigation={[
+            "This is a button",
+            "Second",
+            "Third"
+          ]}
+          searchBar={<SearchBar />}
+          options={[
+            <Icons.MenuIcon key='1' />,
+            <Icons.Language key='2' />
+          ]}
+          endButton={"Login"}
+        />
+
       </main>
-    </>
+    </React.Fragment>
   )
 }
